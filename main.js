cf5606bcc1bdb33f6da5c925adf51cab70839b4d
BBQ <barbeku@pm.me>
2023-08-14 10:22:18 +0300

    fix: CORS error

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ fileInp.addEventListener("change", async e => {
 
 function fetchRequest(file, formData) {
     infoText.innerText = "Scanning QR Code...";
-    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    fetch("https://api.qrserver.com/v1/read-qr-code/", {
         method: 'POST', body: formData
     }).then(res => res.json()).then(result => {
         result = result[0].symbol[0].data;
